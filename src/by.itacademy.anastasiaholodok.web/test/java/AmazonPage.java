14700230b8dbd59c5812795144f3d84b35e0ec2f
NastassiyaKh <nholodok@yandex.ru>
2023-02-15 16:52:17 +0300

    add AmazonPage class

---
+++ src/by.itacademy.anastasiaholodok.web/test/java/AmazonPage.java
@@ -0,0 +1,11 @@
+public class AmazonPage {
+    public static final String URL = "https://www.amazon.com/";
+    public static final String COPYRIGHT = "//*[@id=\"navFooter\"]/div[5]/span";
+    public static final String BUTTON_CART = "//*[@id=\"nav-cart\"]";
+    public static final String LABEL_WITHOUT_FILLING_CART = "//*[@id=\"sc-active-cart\"]/div/div/div[2]/div[1]/h2";
+    public static final String BUTTON_MODAL_LOGIN = "//*[@id=\"a-autoid-0-announce\"]/span";
+    public static final String LOGO_LOGIN_FORM = "//*[@id=\"authportal-main-section\"]/div[2]/div/div[1]/form/div/div/div/h1";
+
+    public static final String BUTTON_CONTINUE = "//*[@id=\"continue\"]";
+    public static final String LABEL_WITHOUT_AUTHORIZATION = "//*[@id=\"auth-email-missing-alert\"]/div/div";
+}
