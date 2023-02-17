760255c8109e5b632e4ee6be381ba05b90b860f1
NastassiyaKh <nholodok@yandex.ru>
2023-02-17 17:59:41 +0300

    refactor OnlinerTest class

--- src/by.itacademy.anastasiaholodok.web/test/java/OnlinerTest.java
+++ src/by.itacademy.anastasiaholodok.web/test/java/OnlinerTest.java
@@ -1,27 +1,34 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class OnlinerTest {
+    WebDriver driver = new ChromeDriver();
+    @Before
+    public void start() {
+        driver.get(OnlinerPage.URL);
+    }
+
     @Test
     public void testOpenOnliner() {
-        WebDriver driver = new ChromeDriver();
-        driver.get(OnlinerPage.URL);
         String copyrightXPath = OnlinerPage.COPYRIGHT;
         By copyrightBy = By.xpath(copyrightXPath);
         WebElement copyrightElement = driver.findElement(copyrightBy);
 
         Assert.assertEquals("© 2001—2023 Onlíner", copyrightElement.getText());
-        driver.quit();
     }
+
     @Test
     public void testOpenOnlinerLoginForm() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get(OnlinerPage.URL);
-
         String btnEnterXPath = OnlinerPage.BUTTON_LOGIN;
         By btnEnterBy = By.xpath(btnEnterXPath);
         WebElement btnEnterElement = driver.findElement(btnEnterBy);
@@ -32,13 +39,10 @@ public class OnlinerTest {
         WebElement logoEnterElement = driver.findElement(logoEnterBy);
 
         Assert.assertEquals("Вход", logoEnterElement.getText());
-        driver.quit();
     }
-    @Test
-    public void testOnlinerLoginFormWithEmptyCredentials() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get(OnlinerPage.URL);
 
+    @Test
+    public void testOnlinerLoginFormWithEmptyCredentials() {
         String btnEnterXPath = OnlinerPage.BUTTON_LOGIN;
         By btnEnterBy = By.xpath(btnEnterXPath);
         WebElement btnEnterElement = driver.findElement(btnEnterBy);
@@ -49,17 +53,16 @@ public class OnlinerTest {
         WebElement btnModalLoginElement = driver.findElement(btnModalLoginBy);
         btnModalLoginElement.click();
 
-        Thread.sleep(5000);
+        WebElement webEl = new WebDriverWait(driver, Duration.ofSeconds(1))
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath(OnlinerPage.LABEL_WITHOUT_PASSWORD)));
 
         Assert.assertEquals("Укажите пароль", driver.findElement(By.xpath(OnlinerPage.LABEL_WITHOUT_PASSWORD)).getText());
         Assert.assertEquals("Укажите ник или e-mail", driver.findElement(By.xpath(OnlinerPage.LABEL_WITHOUT_EMAIL)).getText());
-        driver.quit();
     }
-    @Test
-    public void testOnlinerLoginFormWithEmptyPassword() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get(OnlinerPage.URL);
 
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword(){
         String btnEnterXPath = OnlinerPage.BUTTON_LOGIN;
         By btnEnterBy = By.xpath(btnEnterXPath);
         WebElement btnEnterElement = driver.findElement(btnEnterBy);
@@ -72,9 +75,34 @@ public class OnlinerTest {
         WebElement btnModalLoginElement = driver.findElement(btnModalLoginBy);
         btnModalLoginElement.click();
 
-        Thread.sleep(5000);
+       new WebDriverWait(driver, Duration.ofSeconds(1))
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath(OnlinerPage.LABEL_WITHOUT_PASSWORD)));
 
         Assert.assertEquals("Укажите пароль", driver.findElement(By.xpath(OnlinerPage.LABEL_WITHOUT_PASSWORD)).getText());
+    }
+    @Test
+    public void testOnlinerLoginFormWithEmptyEmail(){
+        String btnEnterXPath = OnlinerPage.BUTTON_LOGIN;
+        By btnEnterBy = By.xpath(btnEnterXPath);
+        WebElement btnEnterElement = driver.findElement(btnEnterBy);
+        btnEnterElement.click();
+
+        driver.findElement(By.xpath(OnlinerPage.INPUT_PASSWORD)).sendKeys("12345");
+
+        String btnModalLoginXPath = OnlinerPage.BUTTON_MODAL_LOGIN;
+        By btnModalLoginBy = By.xpath(btnModalLoginXPath);
+        WebElement btnModalLoginElement = driver.findElement(btnModalLoginBy);
+        btnModalLoginElement.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(1))
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath(OnlinerPage.LABEL_WITHOUT_EMAIL)));
+
+        Assert.assertEquals("Укажите ник или e-mail", driver.findElement(By.xpath(OnlinerPage.LABEL_WITHOUT_EMAIL)).getText());
+    }
+    @After
+    public void finish(){
         driver.quit();
     }
 }
